516fad359600a3e5d2e6fae2fee802fd2c0756ea
Noruwa-Kenneth-stack <noruwa19@gmail.com>
2025-12-18 20:04:21 +0100

    border

--- src/components/AdminLoginModal.tsx
+++ src/components/AdminLoginModal.tsx
@@ -34,7 +34,7 @@ export default function AdminLoginModal({ isOpen, onClose }: Props) {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             className="w-full px-4 py-2 border border-gray-200  rounded-lg text-black
-    focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors transition-shadow duration-500 ease-in-out"
+     transition-colors transition-shadow duration-500 ease-in-out"
             required
           />
           <input
